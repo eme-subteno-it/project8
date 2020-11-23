c352c94fa4155492f5d3f1ede400f8aa7e8c0869
Elodie Meunier <eme@subteno-it.com>
2020-11-23 12:00:16 +0100

    [FIX] - retry secret key settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,9 +25,6 @@ def gettext_noop(s):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'boubou'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
