8a102a44ee493166cbc9927d0e84767e10330f41
Elodie Meunier <eme@subteno-it.com>
2020-12-07 12:20:59 +0100

    [ADD] - test for api openfoodfacts

--- project/api_food/tests/test_command.py
+++ project/api_food/tests/test_command.py
@@ -1,9 +1,57 @@
-from django.test import TestCase
+from io import StringIO
+from django.test import TestCase, override_settings
 from django.core.management import call_command
-from api_food.api import ApiFood
+from product.models import Product, Category
 
 
 class CommandTest(TestCase):
 
-    def test_handle(self):
-        pass
+    def setUp(self):
+        self.category = Category.objects.create(name='Boisson', api_id='en:dried-products-to-be-rehydrated')
+        self.product = Product.objects.create(
+            name='Jus d\'abricot',
+            description='description',
+            store='Auchan',
+            nutriscore='1',
+            nutriscore_grade='A',
+            image='https://static.openfoodfacts.org/images/products/322/885/700/0852/front_fr.134.400.jpg',
+            url_api='https://fr.openfoodfacts.org/produit/3502110009449/pur-jus-d-orange-sans-pulpe-tropicana',
+            fat_level='low',
+            satured_fat_level='low',
+            sugars_level='moderate',
+            salt_level='low',
+            fat_g=0,
+            satured_fat_g=0,
+            sugars_g=8.9,
+            salt_g=0
+        )
+        self.product.category_ids.add(self.category)
+
+    def call_command(self, *args, **kwargs):
+        out = StringIO()
+        call_command(
+            "populate_db",
+            *args,
+            stdout=out,
+            stderr=StringIO(),
+            **kwargs,
+        )
+        return out.getvalue()
+
+    @override_settings(LANGUAGE_CODE='en-US', LANGUAGES=(('en', 'English'),))
+    def test_delete_db(self):
+        out = self.call_command('--delete')
+        self.assertEqual(out, 'The products and their categories have been deleted\n')
+
+    @override_settings(LANGUAGE_CODE='en-US', LANGUAGES=(('en', 'English'),))
+    def test_delete_db_without_products(self):
+        # Delete the database before for the test
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        out = self.call_command('--delete')
+        self.assertEqual(out, 'The database is already empty.\n')
+
+    def test_populate_db(self):
+        out = self.call_command()
+        self.assertEqual(out, 'Add the categories from api OpenfoodFact\n')
