42410c89dbf46e731dc5324060ac0cd902b5ceb9
Elodie Meunier <eme@subteno-it.com>
2021-03-12 16:27:40 +0100

    [FIX] - fix config for the smtp server mail

--- project/project/settings/config.py
+++ project/project/settings/config.py
@@ -187,5 +187,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['USER_EMAIL']
-EMAIL_HOST_PASSWORD = os.environ['PASS_EMAIL']
+
+if os.environ.get('ENV'):
+    EMAIL_HOST_USER = os.environ['USER_EMAIL']
+    EMAIL_HOST_PASSWORD = os.environ['PASS_EMAIL']
