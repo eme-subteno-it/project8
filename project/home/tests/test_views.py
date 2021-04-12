95cfd22f5d02f66637aea2882c044c725d54cf0e
Elodie Meunier <eme@subteno.com>
2021-04-12 17:49:18 +0200

    [FIX] - Fix trads

--- project/home/tests/test_views.py
+++ project/home/tests/test_views.py
@@ -11,11 +11,19 @@ class IndexViewTest(TestCase):
 
     def test_lang_fr(self):
         response = self.client.get('/', HTTP_ACCEPT_LANGUAGE='fr')
-        self.assertContains(response, '<h1 class="text-uppercase text-white font-weight-bold">Du gras, oui, mais de qualité !</h1>', html=True)
+        self.assertContains(
+            response,
+            '<h1 class="text-uppercase text-white font-weight-bold">Du gras, oui, mais de qualité !</h1>',
+            html=True
+        )
 
     def test_lang_en(self):
         response = self.client.get('/', HTTP_ACCEPT_LANGUAGE='en')
-        self.assertContains(response, '<h1 class="text-uppercase text-white font-weight-bold">Fat, yes, but of quality!</h1>', html=True)
+        self.assertContains(
+            response,
+            '<h1 class="text-uppercase text-white font-weight-bold">Fat, yes, but of quality!</h1>',
+            html=True
+        )
 
 
 class LegalNoticeViewTest(TestCase):
